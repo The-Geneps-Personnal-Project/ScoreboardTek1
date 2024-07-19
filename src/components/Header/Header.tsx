d35f3ecafc6d7a3f5fc3b7e384b45aa353764570
Julien Delphine <julien.delphine@epitech.eu>
2024-07-19 16:07:34 +0200

    ✨ feat(header): update header with new assets

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,10 @@ import React from "react";
 
 import "./Header.css";
 
+import header from "../../assets/Header.png";
+
 export const Header: React.FC<React.HTMLAttributes<HTMLDivElement>> = () => {
-  return <div className="Header">Scoreboard</div>;
+  return (
+    <div className="Header" style={{ backgroundImage: `url(${header})` }}></div>
+  );
 };
